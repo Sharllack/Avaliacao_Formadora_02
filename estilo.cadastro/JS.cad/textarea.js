d6da1e1dc3ed030ce7951362d1be3a22f85f00f7
Sharllack <164765027+Sharllack@users.noreply.github.com>
2024-04-27 14:27:34 -0300

    smndnfvbdvbsadjv,sd

--- estilo.cadastro/JS.cad/textarea.js
+++ estilo.cadastro/JS.cad/textarea.js
@@ -1,3 +1,5 @@
-let textarea = document.getElementsByTagName('textarea');
-
-console.log(textarea);
+function atualizarContador() {
+    let text = document.getElementById('idobs').value;
+    let cont = text.length;
+    document.getElementById('cont').innerHTML = cont + '/200';
+}
